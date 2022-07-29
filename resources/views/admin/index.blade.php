a71fc9d02f57d3b24229e5949315daaa0edd1bef
Leonel Goday <goday985@gmail.com>
2022-07-29 08:53:10 -0300

    add total filter

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -196,7 +196,7 @@ Admin Panel
                 "zeroRecords": "UPS!!! no hay registros",
                 "info": "Mostrando pág. _PAGE_ de _PAGES_",
                 "infoEmpty": "No hay registros disponibles",
-                "infoFiltered": "(Filtrando de un total de _MAX_ records)",
+                "infoFiltered": "(Filtrando _TOTAL_ de un total de _MAX_ records)",
                 "search": "Buscar",
                 "paginate": {
                     "first":      "Primer",
