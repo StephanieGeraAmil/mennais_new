5b510e021becf575e3968128a1b6b32b95f30110
Leonel Goday <goday985@gmail.com>
2022-08-10 17:19:08 -0300

    remove group inscription button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,7 +63,7 @@
 @if (Carbon\Carbon::now()->lt(Carbon\Carbon::parse('29-09-2022'))) 
 <div align="center">
   <a href="/simple_inscription" class="custom-page-typo-item u-active-custom-color-22 u-border-2 u-border-active-palette-1-light-2 u-border-hover-palette-1-dark-1 u-border-palette-1-dark-1 u-btn u-button-style u-hover-palette-1-dark-1 u-palette-1-light-3 u-btn-1">inscripción individual</a>
-  <a href="/group_inscription" class="custom-page-typo-item u-active-custom-color-22 u-border-2 u-border-active-palette-1-light-2 u-border-hover-palette-1-dark-1 u-border-palette-1-dark-1 u-btn u-button-style u-hover-palette-1-dark-1 u-palette-1-light-3 u-btn-2">inscripción Grupal</a>
+  {{-- <a href="/group_inscription" class="custom-page-typo-item u-active-custom-color-22 u-border-2 u-border-active-palette-1-light-2 u-border-hover-palette-1-dark-1 u-border-palette-1-dark-1 u-btn u-button-style u-hover-palette-1-dark-1 u-palette-1-light-3 u-btn-2">inscripción Grupal</a> --}}
 </div>
 @else
 <p class="u-text u-text-5">Inscripción finalizada.</p>
