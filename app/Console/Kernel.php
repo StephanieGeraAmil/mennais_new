d5b08a11b9313349caf07260834078ae6553b34c
Leonel Goday <goday985@gmail.com>
2022-09-23 16:56:25 -0300

    add hour test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('store:loginfo')->weeklyOn(5, '16:50');
-        $schedule->command('store:loginfo')->weeklyOn(5, '19:51');
+        $schedule->command('store:loginfo')->weeklyOn(5, '16:58');
+        $schedule->command('store:loginfo')->weeklyOn(5, '19:59');
 //        $schedule->command('store:loginfo')->everyMinute();
     }
 
