482a1b1252f098f92fbeb6ca1d2fa1452d7aba5d
Leonel Goday <goday985@gmail.com>
2022-09-28 10:58:58 -0300

    test alert

--- resources/views/emails/alert.blade.php
+++ resources/views/emails/alert.blade.php
@@ -1,6 +1,6 @@
 <p>Hola {{$inscription->userData->name}} {{$inscription->userData->lastname}},</p>
 <p>
-    Le informamos que el ingreso al Colegio Stella Maris para la visita del Workshop de AUDEC es por la calle Kelly 2269 entre Máximo Tajes y Sudriers - Edificio Christian Kids.
+    Le informamos que el ingreso al Colegio Stella Maris para la visita del Workshop de AUDEC para el día de hoy, miércoles, es por la calle Kelly 2269 entre Máximo Tajes y Sudriers - Edificio Christian Kids.
 </p>
 <p>
     Saludos,
