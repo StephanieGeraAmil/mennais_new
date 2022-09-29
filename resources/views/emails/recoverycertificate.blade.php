db629e91c81af0abac116b5e3fdcf83c6d0ecdf2
Leonel Goday <goday985@gmail.com>
2022-09-29 15:59:41 -0300

    send with event name

--- resources/views/emails/recoverycertificate.blade.php
+++ resources/views/emails/recoverycertificate.blade.php
@@ -1,6 +1,6 @@
 <p>Hola {{$inscription->userData->name}} {{$inscription->userData->lastname}},</p>
 <p>
-Gracias por asistir a Workshop Audec.<BR/>
+Gracias por asistir a {{env('EVENT_NAME')}}.<BR/>
 En el siguiente link podrá descargar su certificado de asistencia: {!!$inscription->certificateUrl()!!}
 </p>
 <p>
