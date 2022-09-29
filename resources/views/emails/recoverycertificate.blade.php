7f382a01841acac9bde8d554f95c801a18525f33
Leonel Goday <goday985@gmail.com>
2022-09-29 17:36:00 -0300

    recovery mail.

--- resources/views/emails/recoverycertificate.blade.php
+++ resources/views/emails/recoverycertificate.blade.php
@@ -1,11 +1,15 @@
 <p>Hola {{$inscription->userData->name}} {{$inscription->userData->lastname}},</p>
 <p>
-Gracias por asistir a {{env('EVENT_NAME')}}.<BR/>
-En el siguiente link podrá descargar su certificado de asistencia: {!!$inscription->certificateUrl()!!}
+    Gracias por asistir a {{env('EVENT_NAME')}}.<BR/>
+    En el siguiente link podrá descargar su certificado de asistencia: {!!$inscription->certificateUrl()!!}
 </p>
 <p>
-Saludos,<BR/>
-AUDEC
+    Le solicitamos completar este breve formulario de evaluación, con el objetivo de seguir mejorando y acercando propuestas de valor.<BR/>
+    COMPLETAR AQUÍ:  <a href="https://forms.gle/RZUTkgc1d3aTJemR7" target="_blank">https://forms.gle/RZUTkgc1d3aTJemR7</a>
+</p>
+<p>
+    Saludos,<BR/>
+    AUDEC
 </p>
 
 
