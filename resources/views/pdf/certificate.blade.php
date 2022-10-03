72d096a1edc46b8323cff6226c2685a6a7419709
Leonel Goday <goday985@gmail.com>
2022-10-03 19:17:41 -0300

    cerreccion septiembre

--- resources/views/pdf/certificate.blade.php
+++ resources/views/pdf/certificate.blade.php
@@ -15,7 +15,7 @@
 </head>
 <body style="width: 1024px; font-size:24px; font-family:'Open Sans',sans-serif"; >
     <div style="width:934px; margin-top:260px; text-align:right">
-        Montevideo, Viernes 30 de setiembre de 2022
+        Montevideo, Viernes 30 de septiembre de 2022
         </div>
     <div style="margin-left:80px; margin-top:49px">
         Se certifica que:
