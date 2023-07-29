fa4639eee51f3fdf53c88fa4ff7a8d4327a3480b
Leonel Goday <goday985@gmail.com>
2023-07-29 19:10:37 -0300

    cambio de titulo

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -1,6 +1,6 @@
 <p>Hola {{$inscription->userData->name}} {{$inscription->userData->lastname}},</p>
 <p>
-    Le recordamos que está inscripto al  {!!env("EVENT_NAME", "Workshop VIVENCIANDO LA TRANSFORMACIÓN EDUCATIVA")!!} el próximo miércoles 2 de agosto a las 18:30 hs.
+    Le recordamos que está inscripto al  {!!env("EVENT_NAME", "La Transformación Educativa en acción")!!} el próximo miércoles 2 de agosto a las 18:30 hs.
 </p>
 <p>
     <a href="{!!$inscription->url()!!}" target="_blank" >Link de acceso</a>
