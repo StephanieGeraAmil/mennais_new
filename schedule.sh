682087cad492a0c2f00a93f5c0c0554433e6fdab
Leonel Goday <goday985@gmail.com>
2023-07-29 16:10:05 -0300

    add schedule in line

--- schedule.sh
+++ schedule.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-cd /home/u370571502/domains/audec.org/public_html
-pwd
-php artisan schedule:run
+cd /home/u370571502/domains/audec.org/public_html && php artisan schedule:run >> /dev/null 2>&1
