5528ebf76cacdf7f256cad91d6e26b8e1d36d217
Stephanie <stephanie.gera.amil@gmail.com>
2024-02-06 09:05:16 -0300

    Update reminder.blade.php
    adding missing info to email

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -1,4 +1,12 @@
+<p>Hola {{$inscription->userData->name}} {{$inscription->userData->lastname}},</p>
+<p>
+  Le recordamos que está inscripto PROEDUCAR XXXII - HACIA UNA INNOVACIÓN CON SENTIDO.
+</p>
+
 <p>Lo esperamos el próximo jueves 8 de febrero a las 8:00 HS en el SALÓN DE ACTOS DEL COLEGIO SANTO DOMINGO  - PALMAR 2288 ESQ. ACEVEDO DÍAZ.</p>
+<p>
+  <!--  <img src="{!!$message->embedData(QrCode::format('png')->generate($inscription->qrUrl()), 'QrCode.png', 'image/png')!!}">-->
+</p>
 <p>
     Saludos,
 </p>
