b851ef0c57c845694262475d021a487b8e5dde0d
Stephanie <stephanie.gera.amil@gmail.com>
2024-02-06 13:38:09 -0300

    Update reminder.blade.php
    fixing qr on email

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -1,16 +1,7 @@
 <p>Hola {{$inscription->userData->name}} {{$inscription->userData->lastname}},</p>
-<p>
-  Le recordamos que está inscripto PROEDUCAR XXXII - HACIA UNA INNOVACIÓN CON SENTIDO.
-</p>
-
+<p>Le recordamos que está inscripto PROEDUCAR XXXII - HACIA UNA INNOVACIÓN CON SENTIDO.</p>
 <p>Lo esperamos el próximo jueves 8 de febrero a las 8:00 HS en el SALÓN DE ACTOS DEL COLEGIO SANTO DOMINGO  - PALMAR 2288 ESQ. ACEVEDO DÍAZ.</p>
-<p>
-  <!--  <img src="{!!$message->embedData(QrCode::format('png')->generate($inscription->qrUrl()), 'QrCode.png', 'image/png')!!}">-->
-    <img src="data:image/png;base64,{{ $qrCodeData }}" alt="QR Code">
-</p>
-<p>
-    Saludos,
-</p>
-<p>
-    AUDEC.
-</p>
+
+<img src="{!!$message->embedData(base64_decode($qrCodeData), 'QrCode.png', 'image/png')!!}">
+<p>Saludos,</p>
+<p>AUDEC.</p>
