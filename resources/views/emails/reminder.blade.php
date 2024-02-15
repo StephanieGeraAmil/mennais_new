0b3c5139e26d9b64b4bdd8a99db211607a94db8a
Stephanie <stephanie.gera.amil@gmail.com>
2024-02-15 17:32:14 -0300

    Update reminder.blade.php
    adding style

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -9,7 +9,7 @@
          <li> Miércoles 21 de febrero a las 18:00 hs - Segunda Sesión virtual</li>
     </ul>
 </p>
-<p>Podrá acceder a las dos sesiones unos minutos antes del comienzo a través del <a href="{!!$inscription->url()!!}" target="_blank">mismo link</a></p>
+<p>Podrá acceder a las dos sesiones unos minutos antes del comienzo a través del <a href="{!!$inscription->url()!!}" target="_blank" style="font-weight: bold; text-decoration: underline;">mismo link</a></p>
 
 <p>Para más información sobre las sesiones y los oradores puede visitar: <a href="audec.org">audec.org</a></p>
 
