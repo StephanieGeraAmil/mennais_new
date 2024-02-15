984e9eb3faa8c48ef04b2bbeb01d6960c1d3383d
Stephanie <stephanie.gera.amil@gmail.com>
2024-02-15 17:14:00 -0300

    Update reminder.blade.php

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -5,13 +5,13 @@
 <p>Le recordamos las próximas actividades del PROEDUCAR XXXII</p>
 <p>
     <ul>
-         <li> - Lunes 19 de febrero a las 18:00 hs - Primera Sesión Virtual </li>
-         <li> - Miércoles 21 de febrero a las 18:00 hs - Segunda Sesión virtual</li>
+         <li> Lunes 19 de febrero a las 18:00 hs - Primera Sesión Virtual </li>
+         <li> Miércoles 21 de febrero a las 18:00 hs - Segunda Sesión virtual</li>
     </ul>
 </p>
-<p>Podrá acceder a las dos sesiones unos minutos antes del comienzo  a través del siguiente link:<a href="{!!$inscription->url()!!}" target="_blank" >Link de acceso</a></p>
+<p>Podrá acceder a las dos sesiones unos minutos antes del comienzo a través del <a href="{!!$inscription->url()!!}" target="_blank">mismo link</a></p>
 
-<p>Para más información sobre las sesiones y los oradores puede visitar: audec.org</p>
+<p>Para más información sobre las sesiones y los oradores puede visitar: <a href="audec.org">audec.org</a></p>
 
 <p>Lo esperamos,</p>
 
