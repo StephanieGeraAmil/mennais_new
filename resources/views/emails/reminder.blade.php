55d4b829f8c485b1ff06ce3afe9e503e78325fb7
Stephanie <stephanie.gera.amil@gmail.com>
2024-02-15 13:17:47 -0300

    Update reminder.blade.php
    update remainder mail

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -1,7 +1,7 @@
 <p>Hola {{$inscription->userData->name}} {{$inscription->userData->lastname}},</p>
 <p>Le recordamos que está inscripto PROEDUCAR XXXII - HACIA UNA INNOVACIÓN CON SENTIDO.</p>
-<p>Lo esperamos el próximo jueves 8 de febrero a las 8:00 HS en el SALÓN DE ACTOS DEL COLEGIO SANTO DOMINGO  - PALMAR 2288 ESQ. ACEVEDO DÍAZ.</p>
-
-<img src="{!!$message->embedData(base64_decode($qrCodeData), 'QrCode.png', 'image/png')!!}">
+<!--<p>Lo esperamos el próximo jueves 8 de febrero a las 8:00 HS en el SALÓN DE ACTOS DEL COLEGIO SANTO DOMINGO  - PALMAR 2288 ESQ. ACEVEDO DÍAZ.</p>-->
+<p>Lo esperamos el próximo lunes 19 de febrero a las 18:00 HS en la primera sesion viruala través de este link: <a href="{!!$inscription->url()!!}" target="_blank" >Link de acceso</a></p>
+<!--<img src="{!!$message->embedData(base64_decode($qrCodeData), 'QrCode.png', 'image/png')!!}">-->
 <p>Saludos,</p>
 <p>AUDEC.</p>
