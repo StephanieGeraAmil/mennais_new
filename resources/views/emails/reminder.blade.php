ad560773da78b122f21043444a112aa9b4ef62b8
Stephanie <stephanie.gera.amil@gmail.com>
2024-02-15 17:01:33 -0300

    Update reminder.blade.php
    updating remainder mail text

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -1,7 +1,19 @@
 <p>Hola {{$inscription->userData->name}} {{$inscription->userData->lastname}},</p>
-<p>Le recordamos que está inscripto PROEDUCAR XXXII - HACIA UNA INNOVACIÓN CON SENTIDO.</p>
+<!--<p>Le recordamos que está inscripto PROEDUCAR XXXII - HACIA UNA INNOVACIÓN CON SENTIDO.</p>-->
 <!--<p>Lo esperamos el próximo jueves 8 de febrero a las 8:00 HS en el SALÓN DE ACTOS DEL COLEGIO SANTO DOMINGO  - PALMAR 2288 ESQ. ACEVEDO DÍAZ.</p>-->
-<p>Lo esperamos el próximo lunes 19 de febrero a las 18:00 HS en la primera sesion viruala través de este link: <a href="{!!$inscription->url()!!}" target="_blank" >Link de acceso</a></p>
 <!--<img src="{!!$message->embedData(base64_decode($qrCodeData), 'QrCode.png', 'image/png')!!}">-->
-<p>Saludos,</p>
+<p>Le recordamos las próximas actividades del PROEDUCAR XXXII</p>
+<p>
+    <ul>
+         <li> - Lunes 19 de febrero a las 18:00 hs - Primera Sesión Virtual </li>
+         <li> - Miércoles 21 de febrero a las 18:00 hs - Segunda Sesión virtual</li>
+    </ul>
+</p>
+<p>Podrá acceder a las dos sesiones unos minutos antes del comienzo  a través del siguiente link:<a href="{!!$inscription->url()!!}" target="_blank" >Link de acceso</a></p>
+
+<p>Para más información sobre las sesiones y los oradores puede visitar: audec.org</p>
+
+<p>Lo esperamos,</p>
+
+<!--<p>Saludos,</p>-->
 <p>AUDEC.</p>
