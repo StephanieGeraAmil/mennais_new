2b6bea09a253c652694e7829089bb31c7db97193
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2024-02-20 12:28:01 -0300

    changing the body of the mail sent for the atendees to download the certificate

--- resources/views/emails/recoverycertificate.blade.php
+++ resources/views/emails/recoverycertificate.blade.php
@@ -1,7 +1,7 @@
-<p>Hola {{$inscription->userData->name}} {{$inscription->userData->lastname}}, Agradecemos su participación en los encuentros LA TRANSFORMACIÓN EDUCATIVA EN ACCIÓN con Melina Furman.</p>
-<p>
+<p>Hola {{$inscription->userData->name}} {{$inscription->userData->lastname}}, Agradecemos su participación en el PROEDUCAR XXXII - HACIA UNA INNOVACIÓN CON SENTIDO.</p>
+<!--<p>
     En este enlace encontrará la grabación de la segunda sesión: <a href="https://www.youtube.com/watch?v=dOBDNueFvZg">https://www.youtube.com/watch?v=dOBDNueFvZg</a>
-</p>
+</p>-->
 <p>
     En el siguiente enlace puede descargarse su  certificado de asistencia: <a href="{!!$inscription->certificateUrl()!!}">{!!$inscription->certificateUrl()!!}</a>
 </p>
@@ -12,3 +12,4 @@
     Equipo de AUDEC<BR/>
     Asociación Uruguaya de Educación Católica
 </p>
+
