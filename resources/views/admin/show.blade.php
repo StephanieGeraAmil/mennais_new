7b6085baa0f663b8f01b7ae879bfd84433d76db9
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2024-06-07 09:20:04 -0300

    changing certificate text

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -69,7 +69,7 @@
                                             </select>
                                         </div>
                                         <div
-                                            class="flex items-center border-b @error('name') border-red-500 @else border-teal-500 @enderror py-2 w-100">
+                                            class="flex items-center border-b @error('institution_name') border-red-500 @else border-teal-500 @enderror py-2 w-100">
                                             <input
                                                 class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
                                                 type="text" placeholder="Institucion" aria-label="Institucion"
@@ -77,7 +77,7 @@
                                                 value="{{ $inscription->userData->institution_name }}" />
                                         </div>
                                         <div
-                                            class="flex items-center border-b @error('document') border-red-500 @else border-teal-500 @enderror py-2 w-100">
+                                            class="flex items-center border-b @error('institution_type') border-red-500 @else border-teal-500 @enderror py-2 w-100">
                                             <input
                                                 class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
                                                 type="text" placeholder="Tipo de institución"
--- resources/views/pdf/certificate.blade.php
+++ resources/views/pdf/certificate.blade.php
@@ -15,7 +15,7 @@
 </head>
 <body style="width: 1024px; font-size:20px; font-family:'Open Sans',sans-serif"; >
     <div style="width:934px; margin-top:200px; text-align:right">
-        Montevideo, miércoles 22 de febrero de 2024
+        Montevideo, viernes 26 de julio de 2024
         </div>
     <div style="margin-left:80px; margin-top:49px">
         Se certifica que:
