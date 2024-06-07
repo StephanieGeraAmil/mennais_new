e05b3b7f8608877fa7f653a06c0238e8c2538135
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2024-06-07 15:34:09 -0300

    commenting text on certificate mail and changing site nn certificateurl

--- resources/views/emails/recoverycertificate.blade.php
+++ resources/views/emails/recoverycertificate.blade.php
@@ -3,12 +3,12 @@
 <p>
     En el siguiente enlace puede descargarse su  certificado de asistencia: <a href="{!!$inscription->certificateUrl()!!}">{!!$inscription->certificateUrl()!!}</a>
 </p>
-<p>
+{{-- <p>
     También compartimos los materiales que nos hicieron llegar los oradores: <a href=" https://audec.edu.uy/index.php/category/materiales/"> https://audec.edu.uy/index.php/category/materiales/</a>
 </p>
 <p>
    A partir de la semana próxima encontrarán las grabaciones de los encuentros Zoom en nuestro canal de Youtube <a href=" https://www.youtube.com/@audec-asociacionuruguayade8401"> https://www.youtube.com/@audec-asociacionuruguayade8401</a>
-</p>
+</p> --}}
 
 
 <p>
