ead9c50e4be5e171023e80346bd6322b6651df24
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2024-06-07 15:44:37 -0300

    changing subject

--- app/Mail/RecoveryCertificateMail.php
+++ app/Mail/RecoveryCertificateMail.php
@@ -31,7 +31,7 @@ class RecoveryCertificateMail extends Mailable
      */
     public function build()
     {
-        return $this->subject(' PROEDUCAR XXXIII - LA GESTIÓN COMO PALABRA- Certificado, materiales y videos')->view('emails.recoverycertificate')
+        return $this->subject(' PROEDUCAR XXXIII - LA GESTIÓN COMO PALABRA- Certificado')->view('emails.recoverycertificate')
         ->with('inscription',$this->inscription);
     }
 }
