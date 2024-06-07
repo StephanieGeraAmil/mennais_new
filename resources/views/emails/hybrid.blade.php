9e20fd5f67ee4be131bb09f4e8ce6f9c7abd6bde
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2024-06-07 12:13:51 -0300

    fixing mail text

--- resources/views/emails/hybrid.blade.php
+++ resources/views/emails/hybrid.blade.php
@@ -1,13 +1,13 @@
 <p>Gracias {{$inscription->userData->name}} por inscribirse al {{env('EVENT_NAME')}}</p>
 <p>
-    El día de la Jornada Presencial deberá presentar el siguiente código QR podrá ingresar al evento.
+    El día de la Jornada deberá presentar el siguiente código QR podrá ingresar al evento.
 </p>
 <p>
     <img src="{!!$message->embedData(QrCode::format('png')->generate($inscription->qrUrl()), 'QrCode.png', 'image/png')!!}">
 </p>
-<p>
+{{-- <p>
 Los días de las sesiones virtuales, a la hora del evento, podrá conectarse a través de este link: <a href="{!!$inscription->url()!!}" target="_blank" >Link de acceso</a>
-</p>
+</p> --}}
 <p>
     Saludos,
 </p>
