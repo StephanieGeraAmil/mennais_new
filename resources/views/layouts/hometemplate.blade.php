2f586a0622b8bbf7d2b384a39466a38f75b8b7de
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2024-12-04 19:16:24 -0300

    deleting min height

--- resources/views/layouts/hometemplate.blade.php
+++ resources/views/layouts/hometemplate.blade.php
@@ -453,7 +453,7 @@ transformación Educativa">
             </div>
         </div>
     </section>
- <section class="u-clearfix u-container-align-center u-gradient u-section-8" id="sec-968d">
+ <section class="u-clearfix u-container-align-center u-gradient u-section-8 no-min-height" id="sec-968d">
         <div class="u-clearfix u-sheet u-valign-middle-lg u-valign-middle-xl u-sheet-1">
             <div
                 class="custom-expanded u-border-2 u-border-palette-1-light-1 u-container-align-center-lg u-container-align-center-xl u-container-style u-expanded-width-lg u-expanded-width-xl u-group u-palette-1-light-3 u-radius-11 u-shape-round u-group-1">
