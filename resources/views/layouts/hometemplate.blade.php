34966de3ef20926c9346cfbd760a947475e814a8
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2024-12-04 19:33:05 -0300

    fix styles

--- resources/views/layouts/hometemplate.blade.php
+++ resources/views/layouts/hometemplate.blade.php
@@ -456,7 +456,7 @@ transformación Educativa">
  <section class="u-clearfix u-container-align-center u-gradient u-section-8 no-min-height" id="sec-968d">
         <div class="u-clearfix u-sheet u-valign-middle-lg u-valign-middle-xl u-sheet-1 no-min-height">
             <div
-                class=" u-border-2 u-border-palette-1-light-1 u-container-align-center-lg u-container-align-center-xl u-container-style u-expanded-width-lg u-expanded-width-xl u-group u-palette-1-light-3 u-radius-11 u-shape-round  no-min-height">
+                class=" u-border-2 u-border-palette-1-light-1 u-container-align-center-lg u-container-align-center-xl u-container-style  u-palette-1-light-3 u-radius-11 u-shape-round  no-min-height">
                 <div class=" no-space no-min-height">
                     {{-- <div
                         class="white-rectangle"> --}}
