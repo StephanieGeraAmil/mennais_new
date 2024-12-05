bbdeab237c06597684de2cd4aa6a16b0103ef335
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2024-12-05 09:51:14 -0300

    fixing speakers photo dimensions

--- resources/views/layouts/hometemplate.blade.php
+++ resources/views/layouts/hometemplate.blade.php
@@ -519,8 +519,8 @@ transformación Educativa">
                         <div class="u-layout">
                             <div class="u-layout-row">
                                 <div class="u-container-style u-layout-cell u-size-15 u-layout-cell-1">
-                                    <div class="u-container-layout u-valign-top u-container-layout-2 extra-padding">
-                                        <img class="u-align-center u-image u-image-contain u-image-default u-preserve-proportions u-image-1"
+                                    <div class="u-container-layout u-valign-top u-container-layout-2">
+                                        <img class="u-align-center u-image u-image-contain u-image-default u-preserve-proportions u-image-1 speakers-photo"
                                            src="images/exp_llewin.png" alt="" data-image-width="350"
                                             data-image-height="350">
                                     </div>
