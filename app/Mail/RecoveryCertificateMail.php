7f16929bc65d712fb7326b57d8d753eaa014790e
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2024-12-05 17:39:14 -0300

    changing event title

--- app/Mail/RecoveryCertificateMail.php
+++ app/Mail/RecoveryCertificateMail.php
@@ -31,7 +31,7 @@ class RecoveryCertificateMail extends Mailable
      */
     public function build()
     {
-        return $this->subject(' PROEDUCAR XXXIII - LA GESTIÓN COMO PALABRA- Certificado')->view('emails.recoverycertificate')
+        return $this->subject('PROEDUCAR XXXII Hacia una innovación con sentido- Certificado')->view('emails.recoverycertificate')
         ->with('inscription',$this->inscription);
     }
 }
