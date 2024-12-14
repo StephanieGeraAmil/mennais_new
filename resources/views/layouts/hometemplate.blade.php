598e558ff684a55ad1f472720eccc7c33fbe633e
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2024-12-14 14:20:57 -0300

    changing discount date

--- resources/views/layouts/hometemplate.blade.php
+++ resources/views/layouts/hometemplate.blade.php
@@ -128,7 +128,7 @@ CONECTAR PARA EDUCAR">
                     <div class="u-container-layout u-container-layout-4 min-height-rectangle">
                       <h3 class="u-custom-font u-font-raleway u-text u-text-palette-1-dark-2 u-text-1">Participación COMPLETA</h3>
                       <p class="u-text u-text-palette-1-dark-3 u-text-2">Incluye la Jornada PRESENCIAL + Sesión VIRTUAL<br>
-                        <br>$ 1.300 hasta el 30/12, luego $1.500<br>Venta exclusiva por Tickantel
+                        <br>$ 1.300 hasta el 31/12, luego $1.500<br>Venta exclusiva por Tickantel
                       </p>
                       <a href="https://tickantel.com.uy/inicio/espectaculo/40017820/espectaculo/Proeducar%202025?3" class="u-active-palette-1-light-2 u-align-center u-border-2 u-border-active-palette-1-light-2 u-border-hover-palette-1-light-2 u-border-palette-1-light-2 u-btn u-button-style u-hover-palette-1-light-2 u-palette-1-dark-1 u-btn-1" target="_blank">Tickantel </a>
                     </div>
