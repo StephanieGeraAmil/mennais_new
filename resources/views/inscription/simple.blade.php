a52ee82323db5eb440939a3615a792be8e8cf7f5
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-10-04 03:11:46 -0300

    changing a for a button

--- resources/views/inscription/simple.blade.php
+++ resources/views/inscription/simple.blade.php
@@ -137,7 +137,9 @@
    
                 <a href="https://lamennais.edu.uy/cp25"  class="custom-page-typo-item u-active-custom-color-22 u-border-2 u-border-active-palette-1-light-2 u-border-hover-palette-1-dark-1 u-border-palette-1-dark-1 u-btn u-btn-submit u-button-style u-hover-palette-1-dark-1 u-palette-1-light-3 u-btn-1" >Volver</a>
         
-                <a onclick="$(this).closest('form').submit()" class="custom-page-typo-item u-active-custom-color-22 u-border-2 u-border-active-palette-1-light-2 u-border-hover-palette-1-dark-1 u-border-palette-1-dark-1 u-btn u-btn-submit u-button-style u-hover-palette-1-dark-1 u-palette-1-light-3 u-btn-1" >Enviar</a>
+                 <button type="submit" class="custom-page-typo-item u-active-custom-color-22 u-border-2 u-border-active-palette-1-light-2 u-border-hover-palette-1-dark-1 u-border-palette-1-dark-1 u-btn u-btn-submit u-button-style u-hover-palette-1-dark-1 u-palette-1-light-3 u-btn-1">
+        Enviar
+    </button>
         </div>
     </form>
 @endsection
