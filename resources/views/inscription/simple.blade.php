7e67dbb07f2ed211f9e032fb409a3262d87b6e97
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-10-04 22:30:20 -0300

    fixing adjuntar button

--- resources/views/inscription/simple.blade.php
+++ resources/views/inscription/simple.blade.php
@@ -175,55 +175,52 @@
             <label for="file-upload-2d36" class="u-label"
                 >Comprobante de pago</label
             >
-            <div class="u-file-input-wrapper">
-                <a class="u-btn u-button-style u-upload-button"
-                    >Adjuntar</a
-                >
-                <div class="u-file-list">
-                    <div class="u-file-item u-file-template">
-                        <span class="u-file-name u-text"
-                            >FileExample.pdf</span
-                        >
-                        <span
-                            class="u-file-remove u-icon u-text-grey-30 u-icon-1"
-                            >
-                            <svg
-                                class="u-svg-link"
-                                preserveAspectRatio="xMidYMin slice"
-                                viewBox="0 0 51.976 51.976"
-                                style=""
-                            >
-                                <use xlink:href="#svg-312f"></use>
-                            </svg>
-                            <svg
-                                class="u-svg-content"
-                                viewBox="0 0 51.976 51.976"
-                                x="0px"
-                                y="0px"
-                                id="svg-312f"
-                                style="
-                                    enable-background: new 0 0
-                                        51.976 51.976;
-                                "
-                            >
-                                <g>
-                                    <path
-                                        d="M44.373,7.603c-10.137-10.137-26.632-10.138-36.77,0c-10.138,10.138-10.137,26.632,0,36.77s26.632,10.138,36.77,0
-    C54.51,34.235,54.51,17.74,44.373,7.603z M36.241,36.241c-0.781,0.781-2.047,0.781-2.828,0l-7.425-7.425l-7.778,7.778
-    c-0.781,0.781-2.047,0.781-2.828,0c-0.781-0.781-0.781-2.047,0-2.828l7.778-7.778l-7.425-7.425c-0.781-0.781-0.781-2.048,0-2.828
-    c0.781-0.781,2.047-0.781,2.828,0l7.425,7.425l7.071-7.071c0.781-0.781,2.047-0.781,2.828,0c0.781,0.781,0.781,2.047,0,2.828
-    l-7.071,7.071l7.425,7.425C37.022,34.194,37.022,35.46,36.241,36.241z"
-                                    ></path>
-                                </g>
-                            </svg>
-                        </span>
-                    </div>
-                </div>
-                <p
-                    class="u-file-max u-text u-text-grey-30 u-text-3"
-                >
-                    Max File Size: 10 MB
-                </p>
+      
+                            <div class="u-file-input-wrapper">
+                                <a class="u-btn u-button-style u-upload-button"
+                                    >Adjuntar</a
+                                >
+                                <div class="u-file-list">
+                                    <div class="u-file-item u-file-template">
+                                        <span class="u-file-name u-text"
+                                            >FileExample.pdf</span
+                                        ><span
+                                            class="u-file-remove u-icon u-text-grey-30 u-icon-1"
+                                            ><svg
+                                                class="u-svg-link"
+                                                preserveAspectRatio="xMidYMin slice"
+                                                viewBox="0 0 51.976 51.976"
+                                                style=""
+                                            >
+                                                <use
+                                                    xlink:href="#svg-312f"
+                                                ></use></svg><svg
+                                                class="u-svg-content"
+                                                viewBox="0 0 51.976 51.976"
+                                                x="0px"
+                                                y="0px"
+                                                id="svg-312f"
+                                                style="
+                                                    enable-background: new 0 0
+                                                        51.976 51.976;
+                                                "
+                                            >
+                                                <g>
+                                                    <path
+                                                        d="M44.373,7.603c-10.137-10.137-26.632-10.138-36.77,0c-10.138,10.138-10.137,26.632,0,36.77s26.632,10.138,36.77,0
+		C54.51,34.235,54.51,17.74,44.373,7.603z M36.241,36.241c-0.781,0.781-2.047,0.781-2.828,0l-7.425-7.425l-7.778,7.778
+		c-0.781,0.781-2.047,0.781-2.828,0c-0.781-0.781-0.781-2.047,0-2.828l7.778-7.778l-7.425-7.425c-0.781-0.781-0.781-2.048,0-2.828
+		c0.781-0.781,2.047-0.781,2.828,0l7.425,7.425l7.071-7.071c0.781-0.781,2.047-0.781,2.828,0c0.781,0.781,0.781,2.047,0,2.828
+		l-7.071,7.071l7.425,7.425C37.022,34.194,37.022,35.46,36.241,36.241z"
+                                                    ></path>
+                                                </g></svg></span>
+                                    </div>
+                                </div>
+                                <p
+                                    class="u-file-max u-text u-text-grey-30 u-text-3"
+                                >
+                                    Tamaño máximo: 10 MB
+                                </p>
                 <span class="u-file-group">
                     <input
                         type="file"
@@ -231,7 +228,7 @@
                         name="payment_file"
                         class="u-input u-input-rectangle u-text-black"
                         required=""
-                        {{-- accept="IMAGES" --}}
+                        accept="IMAGES"
                 /></span>
             </div>
     </div>
