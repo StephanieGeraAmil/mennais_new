cecb4cd363add8b39e29b58c6b5a2ef6db63a62a
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-10-07 15:58:52 -0300

    fixing recovery certificate form

--- resources/views/recoverycertificate.blade.php
+++ resources/views/recoverycertificate.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.formtemplate')
+{{-- @extends('layouts.formtemplate')
 @section('title')
 <h3 class="u-align-center u-text u-text-1">Solicitud de certificado</h3>
 @endsection
@@ -95,12 +95,116 @@
 </form>
 @endsection
 @section('custom_script')
-{{-- <script>  
+<script>  
     function clean_document(element){
         let input = $(element);
         let input_val = input.val();
         new_input_val = input_val.replace(/\D/g, "");
         input.val(new_input_val);
     }
-</script>     --}}
-@endsection 
+</script>     
+@endsection  --}}
+
+
+
+@extends('layouts.formtemplate')
+@section('title')
+<h3 class="u-align-center u-text u-text-1">Solicitud de certificado</h3>
+@endsection
+@section('notifications')
+    @isset($errors)                                        
+        @error('document')                        
+       <section
+            class="u-clearfix u-palette-2-light-2 u-section-4"
+            id="block-7"
+        >
+            <div class="u-clearfix u-sheet u-sheet-1">
+
+                        <h5 class="u-text u-text-default u-text-1">
+                            El documento ingresado no es correcto.
+                        </h5>                         
+            </div>
+
+        </section>
+        @enderror                        
+    @endisset
+
+    @isset($wrong_document)
+     <section
+            class="u-clearfix u-palette-2-light-2 u-section-4"
+            id="block-7"
+        >
+            <div class="u-clearfix u-sheet u-sheet-1">
+
+                    <h5 class="u-text u-text-default u-text-1">
+                        El documento ingresado no es correcto.
+                    </h5>                         
+
+            </div>
+        </section>
+
+    @endisset
+    @isset($success)
+     <section
+            class="u-clearfix u-palette-2-light-2 u-section-4"
+            id="block-7"
+        >
+        <section class="u-clearfix u-custom-color-1 u-section-5" id="sec-ef85">
+            <div class="u-clearfix u-sheet u-sheet-1">
+                
+                        <h5 class="u-text u-text-default u-text-1">
+                            El vínculo se ha enviado correctamente.
+                        </h5>
+                <p class="u-align-center u-text u-text-1">
+                        <h5 class="u-text u-text-default u-text-1">   El vínculo se ha enviado correctamente.</h5>
+                </p>
+            </div>
+          
+      
+    </section>                        
+    </section>
+                        
+    @endisset
+    @isset($fail)           
+   <section
+            class="u-clearfix u-palette-2-light-2 u-section-4"
+            id="block-7"
+        >
+            <div class="u-clearfix u-sheet u-sheet-1">
+
+                    <h5 class="u-text u-text-default u-text-1">
+                        Usted no figura acreditado, por favor comuníquese con nosotros al teléfono <a href="tel:24099899">2409 9899</a>, al mail <a href="mailto:secretaria@audec.edu.uy?subject=Solicitud%20de%20Certificado"  >secretaria@audec.edu.uy</a>
+                    </h5>                         
+
+            </div>
+
+        </section>                    
+@endisset
+@endsection
+
+@section('form')
+<form class="w-full max-w-sm certificate_form" action="{{Route('inscription.certificateRecoveryMail')}}" method="POST">
+    @csrf
+     <div class="u-form-group u-form-name">
+            <label for="document-072d" class="u-label">Cédula de Identidad</label>
+            <input
+                type="text"
+                placeholder="(1234567-8)"
+                id="document-072d"
+                name="document"
+                class="u-input u-input-rectangle"
+                required=""
+            />
+     </div>	
+
+        </br>
+        </br>
+      <div class="u-align-left u-form-group u-form-submit">
+             <a onclick="$(this).closest('form').submit()"
+                id="submit-btn"
+                class="u-btn u-btn-submit u-button-style u-btn-3"
+                >Enviar</a>
+    </div>
+</form>
+@endsection
+@section('custom_script')
