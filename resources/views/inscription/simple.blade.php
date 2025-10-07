83c9c4f5321eb2f3129ba03364f9e0359e288cc6
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-10-07 14:55:42 -0300

    changing notifications

--- resources/views/inscription/simple.blade.php
+++ resources/views/inscription/simple.blade.php
@@ -275,7 +275,7 @@ document.addEventListener("DOMContentLoaded", function () {
         >
             <div class="u-clearfix u-sheet u-sheet-1">
 
-                        <h5 class="u-text u-text-default u-text-1">
+                        {{-- <h5 class="u-text u-text-default u-text-1">
                             @error('name')
                                 El campo nombre no es valido.<br />
                             @enderror
@@ -305,23 +305,16 @@ document.addEventListener("DOMContentLoaded", function () {
                             @error('payment_file')
                                 El campo comprobante de pago no es correcto.<br />
                             @enderror 
-                            {{-- @if ($errors->any())
-                                <div class="u-size-30">
-                                    <div class="u-layout-col">
-                                        <div class="u-align-center u-container-style u-layout-cell u-palette-2-base u-size-60 u-layout-cell-1">
-                                            <div class="u-container-layout u-valign-middle u-container-layout-1">
-                                                <h5 class="u-text u-text-default u-text-1">
-                                                  
-                                                    @foreach ($errors->all() as $error)
-                                                        <div>{{ $error }}</div>
-                                                    @endforeach
-                                                </h5>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endif --}}
-                        </h5>
+                            </h5> --}}
+                         
+                            <h5 class="u-text u-text-default u-text-1">
+                                
+                                @foreach ($errors->all() as $error)
+                                    <div>{{ $error }}</div>
+                                @endforeach
+                            </h5>
+                              
+                        
              </div>
         </section>
     @endif
