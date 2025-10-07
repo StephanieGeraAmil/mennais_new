d75d19ef1988b5e82afd3c50925ebd5880a4134a
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-10-07 16:20:05 -0300

    fixing form template so it shows info sectioon

--- resources/views/layouts/formtemplate.blade.php
+++ resources/views/layouts/formtemplate.blade.php
@@ -92,7 +92,7 @@
    <section class="u-clearfix u-section-6" id="block-2">
             <div class="u-clearfix u-sheet u-valign-top u-sheet-1">
                 <div class="u-align-center u-form u-form-1">
-                    @section('info')
+                    @yield('info')
                     @yield('form')
                 </div>
             </div>
