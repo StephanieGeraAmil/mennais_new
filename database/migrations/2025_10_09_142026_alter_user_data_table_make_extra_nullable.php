97d0dc98b6d62153920dc759698307f934ec0835
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-10-09 16:22:55 -0300

    chnaging class name

--- database/migrations/2025_10_09_142026_alter_user_data_table_make_extra_nullable.php
+++ database/migrations/2025_10_09_142026_alter_user_data_table_make_extra_nullable.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterUserDataTable extends Migration
+class AlterUserDataTableMakeExtraNullable extends Migration
 {
     /**
      * Run the migrations.
