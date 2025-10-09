0265337091a6098dc9a16639452865ae8facd12e
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-10-09 15:59:57 -0300

    making the new inscription and decrement of the group inscrption avaiability on the same transaction

--- app/Http/Controllers/GroupInscriptionController.php
+++ app/Http/Controllers/GroupInscriptionController.php
@@ -32,66 +32,145 @@ class GroupInscriptionController extends Controller
         return view('inscription.join', compact('group'));
     }
 
-    public function joinStore(Request $request, $id)
-    {
-        // $group = \App\Models\GroupInscription::findOrFail($id);
-        $group = GroupInscription::findOrFail($id);
-       $validated_data = $request->validate([
-            'code' => 'required|string',
-            'type' => 'required|in:virtual,hibrido',
-            'name' => 'required|string|max:255',
-            'email' => 'required|email',
-            'document' => 'nullable|string',
-            'institution_name' => 'nullable|string',
-            'institution_type' => 'nullable|string',
-        ]);
+//     public function joinStore(Request $request, $id)
+//     {
+//         // $group = \App\Models\GroupInscription::findOrFail($id);
+//         $group = GroupInscription::findOrFail($id);
+//        $validated_data = $request->validate([
+//             'code' => 'required|string',
+//             'type' => 'required|in:virtual,hibrido',
+//             'name' => 'required|string|max:255',
+//             'email' => 'required|email',
+//             'document' => 'nullable|string',
+//             'institution_name' => 'nullable|string',
+//             'institution_type' => 'nullable|string',
+//         ]);
 
 
-    //  $useRequest = new GroupCodeUseRequest([
-    //         'group_inscription_id' => $group->id,
-    //         'code' => $validated_data['code'],
-    //         'type' => $validated_data['type'],
-    //     ]);
+//     //  $useRequest = new GroupCodeUseRequest([
+//     //         'group_inscription_id' => $group->id,
+//     //         'code' => $validated_data['code'],
+//     //         'type' => $validated_data['type'],
+//     //     ]);
 
-    // $controller = app(self::class);
-    // $response = $controller->groupInscriptionUse($useRequest);
-        $decrementResult = $this->decrementGroupAvailability($group, $validated_data['code'], $validated_data['type']);
+//     // $controller = app(self::class);
+//     // $response = $controller->groupInscriptionUse($useRequest);
+//         $decrementResult = $this->decrementGroupAvailability($group, $validated_data['code'], $validated_data['type']);
 
-    if ($decrementResult !== true) {
-        return back()->withErrors(['error' => $decrementResult]);
-    }
+//     if ($decrementResult !== true) {
+//         return back()->withErrors(['error' => $decrementResult]);
+//     }
 
-    // if ($response->getStatusCode() !== 200) {
-    //     return back()->withErrors(['error' => json_decode($response->getContent(), true)['error']]);
-    // }   
-$clean_name = preg_replace('/[^A-Za-z0-9\-]/', '_', $request->get('name'));
-     $user_data = UserData::create([
-            'name' => $clean_name,
-            'document' => Arr::get($validated_data, "document"),
-            'email' => $validated_data['email'],
-            'institution_name' => Arr::get($validated_data, 'institution_name'),
-            'institution_type' => Arr::get($validated_data, 'institution_type'),
-        ]);
-         $inscription = Inscription::create([
-            'user_data_id' => $user_data->id,
-            'payment_id' => null,
-            'status' => 1,
-            'type' => $validated_data['type'] === 'hibrido' ? InscriptionTypeEnum::HIBRIDO : InscriptionTypeEnum::REMOTO,
-        ]);
-        try {
-            Mail::to($user_data->email)->send(new FacetofaceInscriptionMail($inscription));   
-            Mail::to(env('ADMIN_EMAIL', "cgerauy@gmail.com"))->send(new AdminInscriptionMail($inscription));     
-            session()->flash('msg', 'Inscripción realizada con exito usando un link de inscripcion grupal!!!');
-        } catch (\Throwable $th) {
-            Log::error("error: ".$th);
-            Log::error("SimpleInscriptionController::Email: ".$user_data->email."; ".env('ADMIN_EMAIL'));
-            session()->flash('msg', 'Inscripción realizada con exito usando un link de inscripcion grupal. En caso de no recibir el email, contactese con Audec');
-        }  
+//     // if ($response->getStatusCode() !== 200) {
+//     //     return back()->withErrors(['error' => json_decode($response->getContent(), true)['error']]);
+//     // }   
+// $clean_name = preg_replace('/[^A-Za-z0-9\-]/', '_', $request->get('name'));
+//      $user_data = UserData::create([
+//             'name' => $clean_name,
+//             'document' => Arr::get($validated_data, "document"),
+//             'email' => $validated_data['email'],
+//             'institution_name' => Arr::get($validated_data, 'institution_name'),
+//             'institution_type' => Arr::get($validated_data, 'institution_type'),
+//         ]);
+//          $inscription = Inscription::create([
+//             'user_data_id' => $user_data->id,
+//             'payment_id' => null,
+//             'status' => 1,
+//             'type' => $validated_data['type'] === 'hibrido' ? InscriptionTypeEnum::HIBRIDO : InscriptionTypeEnum::REMOTO,
+//         ]);
+//         try {
+//             Mail::to($user_data->email)->send(new FacetofaceInscriptionMail($inscription));   
+//             Mail::to(env('ADMIN_EMAIL', "cgerauy@gmail.com"))->send(new AdminInscriptionMail($inscription));     
+//             session()->flash('msg', 'Inscripción realizada con exito usando un link de inscripcion grupal!!!');
+//         } catch (\Throwable $th) {
+//             Log::error("error: ".$th);
+//             Log::error("SimpleInscriptionController::Email: ".$user_data->email."; ".env('ADMIN_EMAIL'));
+//             session()->flash('msg', 'Inscripción realizada con exito usando un link de inscripcion grupal. En caso de no recibir el email, contactese con Audec');
+//         }  
 
-    return redirect()->route('group.inscription.join', ['id' => $id])
-        ->with('success', 'Inscripción completada correctamente.');
-    }   
+//     return redirect()->route('group.inscription.join', ['id' => $id])
+//         ->with('success', 'Inscripción completada correctamente.');
+//     }   
     
+public function joinStore(Request $request, $id)
+{
+    $group = GroupInscription::findOrFail($id);
+
+    $validated_data = $request->validate([
+        'code' => 'required|string',
+        'type' => 'required|in:virtual,hibrido',
+        'name' => 'required|string|max:255',
+        'email' => 'required|email',
+        'document' => 'nullable|string',
+        'institution_name' => 'nullable|string',
+        'institution_type' => 'nullable|string',
+    ]);
+
+    try {
+        DB::transaction(function () use ($group, $validated_data, $request) {
+            Log::info("Decrementing type: {$validated_data['type']} for group {$group->id}");
+
+            $lockedGroup = GroupInscription::where('id', $group->id)->lockForUpdate()->first();
+
+            if ($validated_data['type'] === 'hibrido') {
+                if ($validated_data['code'] !== $lockedGroup->code_hybrid) {
+                    throw new \Exception('Código de inscripción completa incorrecto.');
+                }
+                if ($lockedGroup->quantity_hybrid_avaiable <= 0) {
+                    throw new \Exception('No quedan cupos híbridos disponibles.');
+                }
+                $lockedGroup->decrement('quantity_hybrid_avaiable', 1);
+            } else {
+                if ($validated_data['code'] !== $lockedGroup->code_remote) {
+                    throw new \Exception('Código de inscripción virtual incorrecto.');
+                }
+                if ($lockedGroup->quantity_remote_avaiable <= 0) {
+                    throw new \Exception('No quedan cupos virtuales disponibles.');
+                }
+                $lockedGroup->decrement('quantity_remote_avaiable', 1);
+            }
+
+         
+            $clean_name = preg_replace('/[^A-Za-z0-9\-]/', '_', $validated_data['name']);
+            $user_data = UserData::create([
+                'name' => $clean_name,
+                'document' => Arr::get($validated_data, 'document'),
+                'email' => $validated_data['email'],
+                'institution_name' => Arr::get($validated_data, 'institution_name'),
+                'institution_type' => Arr::get($validated_data, 'institution_type'),
+            ]);
+
+      
+            $inscription = Inscription::create([
+                'user_data_id' => $user_data->id,
+                'payment_id' => null,
+                'status' => 1,
+                'type' => $validated_data['type'] === 'hibrido'
+                    ? InscriptionTypeEnum::HIBRIDO
+                    : InscriptionTypeEnum::REMOTO,
+            ]);
+
+          
+            $request->session()->put('new_inscription_id', $inscription->id);
+            $request->session()->put('new_user_email', $user_data->email);
+        });
+
+      
+        $inscription = Inscription::find(session('new_inscription_id'));
+        $user_email = session('new_user_email');
+
+        Mail::to($user_email)->send(new FacetofaceInscriptionMail($inscription));
+        Mail::to(env('ADMIN_EMAIL', 'cgerauy@gmail.com'))->send(new AdminInscriptionMail($inscription));
+
+        session()->flash('msg', 'Inscripción realizada con éxito usando un link grupal.');
+        return redirect()->route('group.inscription.join', ['id' => $id]);
+
+    } catch (\Exception $e) {
+        Log::error("Error in joinStore: {$e->getMessage()}");
+        return back()->withErrors(['error' => $e->getMessage()]);
+    }
+}
+
     
     public function groupInscriptionStore(GroupInscriptionRequest $request)
     {
