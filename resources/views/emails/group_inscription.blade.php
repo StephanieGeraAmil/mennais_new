74f586549fef159c83231831464b9592b6fab236
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-10-10 14:35:09 -0300

    commenting link on mail

--- resources/views/emails/group_inscription.blade.php
+++ resources/views/emails/group_inscription.blade.php
@@ -5,14 +5,14 @@
 <p><strong>Código para inscripciones híbridas:</strong> {{ $group_inscription->code_hybrid }}</p>
 <p><strong>Código para inscripciones remotas:</strong> {{ $group_inscription->code_remote }}</p>
 
-$joinUrl = route('group.inscription.join', ['id' => $this->groupInscription->id]);
-<p><a href="{{ $joinUrl }}">Link al que deben acceder para completar la inscripcion</a></p>
+{{-- $joinUrl = route('group.inscription.join', ['id' => $this->groupInscription->id]);
+<p><a href="{{ $joinUrl }}">Link al que deben acceder para completar la inscripcion</a></p> --}}
 {{-- <p>
     Ud. puede gestionar sus invitaciones desde el siguiente link:
     {!!$group_inscription->getUrl()!!}<BR/>
 </p> --}}
 
-
+  
 <p>
 Saludos,
 </p>
