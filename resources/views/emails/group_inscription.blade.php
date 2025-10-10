3381c8694e3b00eb416821919a4f5e4bdf2c8911
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-10-10 15:09:44 -0300

    adding control so the code is not sent in the mail if that type of inscrptions were not selected

--- resources/views/emails/group_inscription.blade.php
+++ resources/views/emails/group_inscription.blade.php
@@ -2,8 +2,24 @@
 <p>
    Comparta cada codigo  y link con quien corresponda, recuerde que cada codigo tiene una cantidad asociada de invitaciones, asi que sea cuidadoso con quien los comparte.
 </p>
-<p><strong>Código para inscripciones híbridas:</strong> {{ $group_inscription->code_hybrid }}</p>
-<p><strong>Código para inscripciones remotas:</strong> {{ $group_inscription->code_remote }}</p>
+{{-- <p><strong>Código para inscripciones híbridas:</strong> {{ $group_inscription->code_hybrid }}</p>
+<p><strong>Código para inscripciones remotas:</strong> {{ $group_inscription->code_remote }}</p> --}}
+
+@if($group_inscription->quantity_hybrid > 0)
+    <p>
+        <strong>Invitaciones híbridas disponibles:</strong> {{ $group_inscription->quantity_hybrid }}
+        <br>
+        <strong>Código para inscripciones híbridas:</strong> {{ $group_inscription->code_hybrid }}
+    </p>
+@endif
+
+@if($group_inscription->quantity_remote > 0)
+    <p>
+        <strong>Invitaciones virtuales disponibles:</strong> {{ $group_inscription->quantity_remote }}
+        <br>
+        <strong>Código para inscripciones remotas:</strong> {{ $group_inscription->code_remote }}
+    </p>
+@endif
 
 <p><a href="{{ $joinUrl }}">Link al que deben acceder para completar la inscripción</a></p>
 
