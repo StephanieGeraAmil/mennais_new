a94c1f836c98047393e0b64352095b401483ea07
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-10-14 15:38:33 -0300

    showing the id column of grroup inscriptin on the inscription index if it has that value

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -129,7 +129,7 @@ Admin Panel
                                         </td>
                                         <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                             <p class="text-gray-900 whitespace-no-wrap">
-                                                {!!$registered_user->groupInscription()!!}
+                                            {{ $registered_user->groupInscription?->id }}       
                                             </p>
                                         </td>
                                         <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
