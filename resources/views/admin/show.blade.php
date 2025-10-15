dcc500037fd25ef4f1056d0e41f8d1869ba09b83
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2025-10-15 14:13:45 -0300

    adding padding

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -114,7 +114,7 @@
 
                                             </select>
                                         </div>
-                                        <div class="">
+                                        <div class="pt-3">
                                             {{-- <div class="pt-3"> --}}
                                                 <button type="submit"
                                                     class="bg-gray-400 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-white rounded-xl">
