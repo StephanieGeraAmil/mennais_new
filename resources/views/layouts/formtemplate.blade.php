6b62175a782036b8344f053f44c36750321bcb73
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2026-02-06 15:09:36 -0300

    making it a bit smaller  the logo

--- resources/views/layouts/formtemplate.blade.php
+++ resources/views/layouts/formtemplate.blade.php
@@ -63,7 +63,7 @@
                     class="u-align-center u-image u-logo img"
                     src="/images/logo_sist.png"
                     alt=""
-                    style="max-width:100%; height:auto;"
+                    style="max-width:90%; height:auto;"
                
                 />
             </div>
