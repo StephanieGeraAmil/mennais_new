fc602a30e319498d211f09e250fcb2e84ed2142a
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2026-02-14 05:08:56 -0300

    changing mail text

--- resources/views/emails/recoverycertificate.blade.php
+++ resources/views/emails/recoverycertificate.blade.php
@@ -1,9 +1,12 @@
-<p>Hola {{$inscription->userData->name}},</p>
-<p>Agradecemos su participación en el PROEDUCAR 2026 - COMUNIDADES QUE CUIDAN</p>
-
+<p>Estimado/a {{$inscription->userData->name}},</p>
+<p>Gracias por haber sido parte de PROEDUCAR 2026</p>
 <p>
-    En el siguiente enlace puede descargarse su  certificado de asistencia: <a href="{!!$inscription->certificateUrl()!!}">{!!$inscription->certificateUrl()!!}</a>
+    Compartimos en este enlace los materiales del evento:<a href="https://drive.google.com/drive/folders/1wBG-0a57X-nTsuVSQc3GwhUyZTYaaCdN?usp=sharing"> Link a materiales</a>
 </p>
+<p>
+    Si desea descargar su certificado de asistencia, puede hacerlo <a href="{!!$inscription->certificateUrl()!!}">aqui</a>
+</p>
+<p>Es importante tener en cuenta que el certificado estará disponible únicamente por el plazo de 30 días. Vencido dicho período, el documento no podrá ser emitido nuevamente, por lo que solicitamos realizar la descarga a la mayor brevedad posible. </p>
 {{-- <p>
     También compartimos los materiales que nos hicieron llegar los oradores: <a href=" https://audec.edu.uy/index.php/category/materiales/"> https://audec.edu.uy/index.php/category/materiales/</a>
 </p>
@@ -13,11 +16,10 @@
 
 
 <p>
-    Cordiales saludos;
+Atentos saludos,
 </p>
 <p>
-    Equipo de AUDEC<BR/>
-    Asociación Uruguaya de Educación Católica
+Equipo de AUDEC.
 </p>
 
 
