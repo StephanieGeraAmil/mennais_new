f8af58e6563d8cb2d02dcd4261f8def34ceffcd9
StephanieGeraAmil <stephanie.gera.amil@gmail.com>
2026-06-23 14:10:51 -0300

    changing reminder location and hometemplate

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -1,17 +1,17 @@
 <p>Hola {{$inscription->userData->name}},</p>
 <p>Te reenviamos la información para acceder al evento:</p>
 @if ($inscription->type === \App\Enums\InscriptionTypeEnum::HIBRIDO)
-<p>Jornada Presencial:</p>
-<p>6 de febrero 08:00 h</p>
-<p>Auditorio Nacional Adela Reta - Sodre</p>
+{{-- <p>Jornada Presencial:</p> --}}
+<p>Jueves 16 de julio 08:00 h</p>
+<p>Universidad Católica del Uruguay</p>
 <p>
     <img src="{!!$message->embedData(QrCode::format('png')->generate($inscription->qrUrl()), 'QrCode.png', 'image/png')!!}">
 </p>
 @endif
-<p>Sesión Virtual:</p>
+{{-- <p>Sesión Virtual:</p>
 <p>20 de febrero 14:00 h </p>
 <p>Podrá acceder a las dos sesiones unos minutos antes del comienzo a través del <strong>link</strong> :
- <button style="font-weight: bold; font: inherit; cursor: pointer; color: blue;" onclick="window.open('{!!$inscription->url()!!}', '_blank')">Acceso al Zoom</button></p>
+ <button style="font-weight: bold; font: inherit; cursor: pointer; color: blue;" onclick="window.open('{!!$inscription->url()!!}', '_blank')">Acceso al Zoom</button></p> --}}
 
 <!--<p>Le recordamos que está inscripto PROEDUCAR XXXII - HACIA UNA INNOVACIÓN CON SENTIDO.</p>-->
 <!--<p>Lo esperamos el próximo jueves 8 de febrero a las 8:00 HS en el SALÓN DE ACTOS DEL COLEGIO SANTO DOMINGO  - PALMAR 2288 ESQ. ACEVEDO DÍAZ.</p>-->
